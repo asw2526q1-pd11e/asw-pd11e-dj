4ed7d9a4c78b76ad6521096e6787ee9bcce692c5
PathologicalMiss <toni.gratacos.miralles@gmail.com>
2025-11-03 12:28:54 +0100

    Flake8

--- communities/urls.py
+++ communities/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.community_list, name='community_list'),
-    path('create/', views.community_create, name='community_create'),
+    path("", views.community_list, name="community_list"),
+    path("create/", views.community_create, name="community_create"),
 ]
--- communities/views.py
+++ communities/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
 from .forms import CommunityForm
 
+
 def community_create(request):
     if request.method == "POST":
         form = CommunityForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('community_list')
+            return redirect("community_list")
     else:
         form = CommunityForm()
-    return render(request, 'communities/community_form.html', {'form': form})
+    return render(request, "communities/community_form.html", {"form": form})
+
 
 def community_list(request):
     from .models import Community
+
     communities = Community.objects.all()
-    return render(request, 'communities/community_list.html', {'communities': communities})
+    return render(
+        request,
+        "communities/community_list.html",
+        {"communities": communities}
+    )
