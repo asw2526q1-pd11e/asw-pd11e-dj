5d2e511e1cd6b576930eb501128e9f904a763533
camivsantos <camila.valeria.santos@estudiantat.upc.edu>
2025-11-05 13:55:16 +0100

    Image in post is tranformed to webp format

--- blog/models/post.py
+++ blog/models/post.py
@@ -1,14 +1,15 @@
 import uuid
 import os
+from io import BytesIO
+from django.core.files.base import ContentFile
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from PIL import Image
 
 
 def post_image_path(instance, filename):
-    ext = filename.split(".")[-1]
-    filename = f"{uuid.uuid4()}.{ext}"
-    return os.path.join("post_image", filename)
+    return os.path.join("post_image", f"{uuid.uuid4()}.webp")
 
 
 class Post(models.Model):
@@ -23,7 +24,7 @@ class Post(models.Model):
     communities = models.ManyToManyField(
         "communities.Community",
         through="blog.PostsCommunities",
-        related_name="posts"
+        related_name="posts",
     )
 
     def __str__(self):
@@ -33,7 +34,25 @@ class Post(models.Model):
         return reverse("blog:post_detail", args=[str(self.id)])
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)  # Guarda primero para obtener el ID
+        if self.image and not self.image.name.endswith(".webp"):
+            try:
+                img = Image.open(self.image).convert("RGB")
+
+                buffer = BytesIO()
+                img.save(buffer, format="WEBP", quality=85)
+                buffer.seek(0)
+
+                new_name = f"{uuid.uuid4()}.webp"
+                self.image.save(new_name,
+                                ContentFile(buffer.read()),
+                                save=False)
+                buffer.close()
+            except Exception as e:
+                print(f"⚠️ Error al convertir imagen a WebP: {e}")
+
         if not self.url:
+            super().save(*args, **kwargs)
             self.url = self.get_absolute_url()
             super().save(update_fields=["url"])
+        else:
+            super().save(*args, **kwargs)
