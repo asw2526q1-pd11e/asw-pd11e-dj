6a5a3d46a1a94808713d2e721a5371747d1c61bd
Aina <aina.serra.ferre@estudiantat.upc.edu>
2025-11-11 17:24:21 +0100

    Fer pàgina configuració, i canvair nom correctament.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    banner = models.ImageField(upload_to='banners/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
