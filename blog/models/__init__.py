a891f7f63716fc4df6ec107835390e01831d4ae0
Camipu <camila.santos@titanos.tv>
2025-11-12 20:26:28 +0100

    upvote or downvote once per user, shows in templates

--- blog/models/__init__.py
+++ blog/models/__init__.py
@@ -1,3 +1,4 @@
 from .post import Post  # noqa: F401
 from .posts_communities import PostsCommunities  # noqa: F401
 from .comment import Comment  # noqa: F401
+from .votes import VotePost, VoteComment  # noqa: F401
