3836d58c8c74762ba35fc3c2a49c4a9e166543c0
PathologicalMiss <toni.gratacos.miralles@gmail.com>
2025-11-18 17:01:18 +0100

    GET post/{id}

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .api_views import post_list  # aquí posarem més views API més endavant
+from .api_views import post_detail, post_list
 
 app_name = "blog_api"
 
 urlpatterns = [
     path('posts/', post_list, name='post_list'),
+    path('posts/<int:pk>/', post_detail, name='post_detail'),
 ]
--- blog/api_views.py
+++ blog/api_views.py
@@ -1,10 +1,19 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
 from rest_framework import serializers
+from drf_yasg.utils import swagger_auto_schema
 
-
+# -------------------- SERIALIZER --------------------
 class PostSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(help_text="Títol del post, màxim 200 caràcters")
+    content = serializers.CharField(help_text="Contingut complet del post")
+    author = serializers.CharField(source="author.username", help_text="Nom d'usuari de l'autor")
+    published_date = serializers.DateTimeField(help_text="Data de publicació")
+    votes = serializers.IntegerField(help_text="Número de vots del post")
+    url = serializers.CharField(help_text="URL absoluta del post")
+
     class Meta:
         model = Post
         fields = [
@@ -14,11 +23,31 @@ class PostSerializer(serializers.ModelSerializer):
             'author',
             'published_date',
             'votes',
-            'url']
+            'url'
+        ]
+
+# -------------------- VISTES --------------------
 
 
+@swagger_auto_schema(
+    method='get',
+    operation_description="Retorna la llista de tots els posts",
+    responses={200: PostSerializer(many=True)}
+)
 @api_view(['GET'])
 def post_list(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="Retorna un post concret amb totes les seves dades. Retorna 404 si no existeix.", # noqa E501
+    responses={200: PostSerializer, 404: 'Not found'}
+)
+@api_view(['GET'])
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    serializer = PostSerializer(post)
     return Response(serializer.data)
