81b5fcd6c7f92698cd63a2b4b7ae1c84f565b020
PathologicalMiss <toni.gratacos.miralles@gmail.com>
2025-11-18 17:26:24 +0100

    GET comentaris (tots, nomes 1r nivell i arbre complet

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .api_views import post_detail, post_list
+from .api_views import post_comments, post_comments_root, post_comments_tree, post_detail, post_list # noqa E501
 
 app_name = "blog_api"
 
 urlpatterns = [
     path('posts/', post_list, name='post_list'),
     path('posts/<int:pk>/', post_detail, name='post_detail'),
+    path('posts/<int:pk>/comments/', post_comments, name='post_comments'),
+    path('posts/<int:pk>/comments_tree/',
+         post_comments_tree,
+         name='post_comments_tree'),  # arbre complet
+    path('posts/<int:pk>/comments_root/',
+         post_comments_root,
+         name='post_comments_root'),  # només 1r nivell
 ]
--- blog/api_views.py
+++ blog/api_views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from .models import Post
 from rest_framework import serializers
 from drf_yasg.utils import swagger_auto_schema
+from .models import Comment
+
 
 # -------------------- SERIALIZER --------------------
 
@@ -33,6 +35,52 @@ class PostSerializer(serializers.ModelSerializer):
             'url'
         ]
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(
+        source="author.username",
+        help_text="Nom d'usuari de l'autor del comentari"
+        )
+    image = serializers.ImageField(
+        help_text="URL de la imatge del comentari, si existeix"
+        )
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'parent',
+            'content',
+            'author',
+            'published_date',
+            'votes',
+            'url',
+            'image'
+        ]
+
+
+class CommentTreeSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source="author.username")
+    image = serializers.ImageField(allow_null=True)
+    replies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'content',
+            'author',
+            'published_date',
+            'votes',
+            'image',
+            'replies'
+        ]
+
+    def get_replies(self, obj):
+        children = obj.replies.all().order_by('published_date')
+        serializer = CommentTreeSerializer(children, many=True)
+        return serializer.data
 # -------------------- VISTES --------------------
 
 
@@ -57,4 +105,57 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     serializer = PostSerializer(post)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="Retorna tots els comentaris d'un post concret. Cada comentari inclou autor, contingut, data, vots i URL de la imatge si existeix.", # noqa E501
+    responses={200: CommentSerializer(many=True), 404: 'Not found'}
+)
+@api_view(['GET'])
+def post_comments(request, pk):
+    """
+    GET /posts/{id}/comments/
+    Retorna tots els comentaris del post amb id=pk.
+    """
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post=post).order_by('published_date')
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="Retorna els comentaris d'un post amb l'estructura en arbre (fills dins de replies).", # noqa E501
+    responses={200: CommentTreeSerializer(many=True), 404: 'Not found'}
+)
+@api_view(['GET'])
+def post_comments_tree(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # només els comentaris arrel
+    root_comments = Comment.objects.filter(
+        post=post,
+        parent__isnull=True
+    ).order_by('published_date')
+    serializer = CommentTreeSerializer(root_comments, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="Retorna només els comentaris de primer nivell d'un post.", # noqa E501
+    responses={200: CommentTreeSerializer(many=True), 404: 'Not found'}
+)
+@api_view(['GET'])
+def post_comments_root(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    root_comments = Comment.objects.filter(
+        post=post,
+        parent__isnull=True
+        ).order_by('published_date')
+    serializer = CommentTreeSerializer(root_comments, many=True)
+    # buidem replies 1r nivell
+    for c in serializer.data:
+        c['replies'] = []
     return Response(serializer.data)
