f81cbf161fb045a483b871203802a8158b1d14c6
PathologicalMiss <toni.gratacos.miralles@gmail.com>
2025-12-18 11:50:41 +0100

    allowed front access (cors)

--- asw_pd11e_dj/settings.py
+++ asw_pd11e_dj/settings.py
@@ -249,6 +249,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://editor.swagger.io",
     "https://petstore.swagger.io",
+    "https://front-hqro.onrender.com",
 ]
 
 CORS_ALLOW_HEADERS = [
